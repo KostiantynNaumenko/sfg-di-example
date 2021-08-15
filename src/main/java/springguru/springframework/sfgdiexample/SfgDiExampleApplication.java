f357b8b05a0fa85971039b1ee9664ac80ca8bde2
KostiantynNaumenko <naumenko.kostiantyn@gmail.com>
2021-08-15 21:40:47 +0300

    adding fake Data Source and config using external properties in datasource.properties

--- src/main/java/springguru/springframework/sfgdiexample/SfgDiExampleApplication.java
+++ src/main/java/springguru/springframework/sfgdiexample/SfgDiExampleApplication.java
@@ -14,22 +14,33 @@ public class SfgDiExampleApplication {
 		System.out.println("----- Profiles -----");
 		I18nController i18nController = ctx.getBean(I18nController.class);
 		System.out.println(i18nController.sayGreeting());
+		System.out.println();
 
 		System.out.println("----- Primary -----");
 		MyController myController = ctx.getBean(MyController.class);
 		System.out.println(myController.sayGreeting());
+		System.out.println();
 
 		System.out.println("----- Property -----");
 		PropertyInjectedController propertyInjectedController = ctx.getBean(PropertyInjectedController.class);
 		System.out.println(propertyInjectedController.getGreeting());
+		System.out.println();
 
 		System.out.println("----- Setter -----");
 		SetterInjectedController setterInjectedController = ctx.getBean(SetterInjectedController.class);
 		System.out.println(setterInjectedController.getGreeting());
+		System.out.println();
 
 		System.out.println("----- Constructor -----");
 		ConstructorInjectedController constructorInjectedController = ctx.getBean(ConstructorInjectedController.class);
 		System.out.println(constructorInjectedController.getGreeting());
+		System.out.println();
 
+		System.out.println("----- External Properties -----");
+		FakeDataSource fakeDataSource = ctx.getBean(FakeDataSource.class);
+		System.out.println(fakeDataSource.getUsername());
+		System.out.println(fakeDataSource.getPassword());
+		System.out.println(fakeDataSource.getJdbcurl());
+		System.out.println();
 	}
 }
---
+++ src/main/java/springguru/springframework/sfgdiexample/config/FakeDataSourceConfig.java
@@ -0,0 +1,25 @@
+package springguru.springframework.sfgdiexample.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import springguru.springframework.sfgdiexample.datasource.FakeDataSource;
+
+@Configuration
+@PropertySource("classpath:datasource.properties")
+public class FakeDataSourceConfig {
+
+    @Bean
+    FakeDataSource fakeDataSource(@Value("${guru.username}") String username,
+                                  @Value("${guru.password}") String password,
+                                  @Value("${guru.jdbcurl}") String jdbcurl) {
+
+        FakeDataSource fakeDataSource = new FakeDataSource();
+        fakeDataSource.setUsername(username);
+        fakeDataSource.setPassword(password);
+        fakeDataSource.setJdbcurl(jdbcurl);
+
+        return fakeDataSource;
+    }
+}
